5538fa348a3dce93f58903abf1f7b0b8585e8594
Ecem Erce <beyzaecemerce@gmail.com>
2023-07-24 22:43:25 +0300

    Data preparation was finished.

---
+++ dataPreparation.py
@@ -0,0 +1,35 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor, Normalize, Compose, Resize
+import cv2
+
+class dataPrep(Dataset):
+    def __init__(self, imagePaths,maskPaths, input_transform=None, mask_transform=None):
+        self.imagePaths = imagePaths
+        self.maskPaths= maskPaths
+        self.input_transform = input_transform
+        self.mask_transform = mask_transform
+        
+    def __len__(self):
+        return len(self.imagePaths)
+    
+    def __getitem__(self, idx):
+		# grab the image path from the current index
+        imagePath = self.imagePaths[idx]
+        maskPath = self.maskPaths[idx]
+		# load the image from disk, swap its channels from BGR to RGB,
+		# and read the associated mask from disk in grayscale mode
+        image = cv2.imread(imagePath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(maskPath)
+        # check to see if we are applying any transformations
+        if self.input_transform is not None:
+            # apply the transformations to both image and its mask
+            image = self.input_transform(image)
+
+        if self.mask_transform is not None:
+            # apply the transformations to both image and its mask
+            mask = self.mask_transform(mask)
+        # return a tuple of the image and its mask
+        return (image, mask)
