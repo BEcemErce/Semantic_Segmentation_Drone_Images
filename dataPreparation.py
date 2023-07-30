bcfae70efb47597306e3fb7110e8b8743c87ae83
Ecem Erce <beyzaecemerce@gmail.com>
2023-07-30 22:17:39 +0300

    Labeled image sizes was prepared for the training

--- dataPreparation.py
+++ dataPreparation.py
@@ -3,13 +3,15 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor, Normalize, Compose, Resize
 import cv2
+import numpy as np
 
 class dataPrep(Dataset):
-    def __init__(self, imagePaths,maskPaths, input_transform=None, mask_transform=None):
+    def __init__(self, imagePaths,maskPaths,n_class, input_transform=None, mask_transform=None):
         self.imagePaths = imagePaths
         self.maskPaths= maskPaths
         self.input_transform = input_transform
         self.mask_transform = mask_transform
+        self.n_class=n_class
         
     def __len__(self):
         return len(self.imagePaths)
@@ -32,4 +34,11 @@ class dataPrep(Dataset):
             # apply the transformations to both image and its mask
             mask = self.mask_transform(mask)
         # return a tuple of the image and its mask
-        return (image, mask)
+        mask,_ =torch.max(mask,dim=0)
+        h, w = mask.size()
+        target = torch.zeros(self.n_class, h, w)
+        for c in range(self.n_class):
+            target[c][mask == c] = 1
+        
+
+        return (image, target)
