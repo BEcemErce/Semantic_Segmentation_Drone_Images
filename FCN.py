8045ec5ed00b9ffeab342b55d11de9c39b53bdc1
Ecem Erce <beyzaecemerce@gmail.com>
2023-08-09 21:59:13 +0300

    Started to add WB

--- FCN.py
+++ FCN.py
@@ -43,6 +43,10 @@ class FCN(nn.Module):
         self.bn1     = nn.BatchNorm2d(512)
         self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
         self.bn2     = nn.BatchNorm2d(256)
+
+        # self.deconv1 = nn.ConvTranspose2d(256, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        # self.bn1     = nn.BatchNorm2d(256)
+
         self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
         self.bn3     = nn.BatchNorm2d(128)
         self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
@@ -61,52 +65,44 @@ class FCN(nn.Module):
         ### ENCODER ###
 
         x = F.relu(self.conv1(x))
-        print("conv1",x.shape)
         x = F.relu(self.conv2(x))
-        print("conv2",x.shape)
         x = self.pool1(x)
-        print("pool1_out",x.shape)
 
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
         x = self.pool2(x)
-        print("pool2_out",x.shape)
+
 
         x = F.relu(self.conv5(x))
         x = F.relu(self.conv6(x))
         x = F.relu(self.conv7(x))
         x = self.pool3(x)
-        print("pool3_out",x.shape)
 
         x = F.relu(self.conv8(x))
         x = F.relu(self.conv9(x))
         x = F.relu(self.conv10(x))
         pool4_out = self.pool4(x)
-        print("pool4_out",pool4_out.shape)
 
         x = F.relu(self.conv11(pool4_out))
         x = F.relu(self.conv12(x))
         x = F.relu(self.conv13(x))
-        print("conv13",x.shape)
         pool5_out = self.pool5(x)
 
-        print("pool5_out",pool5_out.shape)
 
-
+        # print("pool3",x.shape)
         score = self.bn1(F.relu(self.deconv1(pool5_out)))     
-        print("bn1",score.shape)
         score = self.bn2(F.relu(self.deconv2(score)))  
-        print("bn2",score.shape)
+        # print("deconv1",score.shape)
 
         score = self.bn3(F.relu(self.deconv3(score)))
-        print("bn3",score.shape)
-
+        # print("deconv3",score.shape)
         score = self.bn4(F.relu(self.deconv4(score)))  
-        print("bn4",score.shape)
+        # print("deconv4",score.shape)
         score = self.bn5(F.relu(self.deconv5(score)))  
-        print("bn5",score.shape)
+        # print("deconv5",score.shape)
         
-        score = self.classifier(score)                    
+        score = self.classifier(score)       
+        # print("classifier",score.shape)             
 
         return score 
 
